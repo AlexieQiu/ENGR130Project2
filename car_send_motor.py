4e8069fda3de03ea719004ca481879a8a26e9c81
Alexie Qiu <qiu235@purdue.edu>
2025-03-07 12:10:32 -0500

    update python files

--- car_send_motor.py
+++ car_send_motor.py
@@ -14,70 +14,74 @@ radio.config(channel=chnl)
 radio.on()
 TIME_OUT = 100000  # Increase time out to see farther, but
 # this will reduce the sample rate
-ECHO = pin1  # ping sensor uses a single pin for ECHO and Trigger
-TRIGGER = pin1
+# ECHO = pin1  # ping sensor uses a single pin for ECHO and Trigger
+# TRIGGER = pin1
 
 
-def distance(tp, ep):
-    # tp is the trigger pin and ep is the echo pin
-    ep.read_digital()  # clear echo
-    tp.write_digital(1)  # Send a 10 microSec pulse
-    sleep_us(10)  # wait 10 microSec
-    tp.write_digital(0)  # Send pulse low
-    ep.read_digital()  # clear echo signal - This is needed for a
-    # pingSensor
-    ts = time_pulse_us(ep, 1, TIME_OUT)  # Wait for echo or time out
-    if ts > 0:
-        ts = ts * 17 // 100  # if system did not timeout, then send
-        # back a scaled value
-    return ts  # Return timeout error as a negative number (-1)
-map = read_map.maze("sectorMap4.txt")
-path = read_map.BFS(map)
-number_of_move = len(path)
-cnt = 0
-record = ""
-while cnt != number_of_move - 1:
-    # tuple for path
-    x1 = path[cnt][0]
-    y1 = path[cnt][1]
-    x2 = path[cnt + 1][0]
-    y2 = path[cnt + 1][1]
+# def distance(tp, ep):
+#     # tp is the trigger pin and ep is the echo pin
+#     ep.read_digital()  # clear echo
+#     tp.write_digital(1)  # Send a 10 microSec pulse
+#     sleep_us(10)  # wait 10 microSec
+#     tp.write_digital(0)  # Send pulse low
+#     ep.read_digital()  # clear echo signal - This is needed for a
+#     # pingSensor
+#     ts = time_pulse_us(ep, 1, TIME_OUT)  # Wait for echo or time out
+#     if ts > 0:
+#         ts = ts * 17 // 100  # if system did not timeout, then send
+#         # back a scaled value
+#     return ts  # Return timeout error as a negative number (-1)
 
-    if x1 < x2 and y1 == y2:
-        display.show(Image.ARROW_S)
-        if record[len(record) - 1] == "W":
-            radio.send("L")
-        elif record[len(record) - 1] == "E":
-            radio.send("R")
-        radio.send("F")
-        record += "S"
-    elif x1 > x2 and y1 == y2:
-        display.show(Image.ARROW_N)
-        if record[len(record) - 1] == "W":
-            radio.send("R")
-        else:
-            radio.send("L")
-        radio.send("F")
-        record += "N"
-    elif y1 > y2 and x1 == x2:
-        display.show(Image.ARROW_W)
-        if record[len(record) - 1] == "N":
-            radio.send("L")
-        else:
-            radio.send("R")
-        radio.send("F")
-        record += "W"
-    elif y1 < y2 and x1 == x2:
-        display.show(Image.ARROW_E)
-        if record[len(record) - 1] == "N":
-            radio.send("R")
-        else:
-            radio.send("L")
-        radio.send("F")
-        record += "E"
-    else:
-        continue
-    sleep(20)
+while True:
+    a = button_a.is_pressed()
+    if a:
+        map = read_map.maze("sectorMap4.txt")
+        path = read_map.BFS(map)
+        number_of_move = len(path)
+        cnt = 0
+        record = ""
+        while cnt != number_of_move - 1:
+            # tuple for path
+            x1 = path[cnt][0]
+            y1 = path[cnt][1]
+            x2 = path[cnt + 1][0]
+            y2 = path[cnt + 1][1]
+
+            if x1 < x2 and y1 == y2:
+                display.show(Image.ARROW_S)
+                if record[len(record) - 1] == "W":
+                    radio.send("L")
+                elif record[len(record) - 1] == "E":
+                    radio.send("R")
+                radio.send("F")
+                record += "S"
+            elif x1 > x2 and y1 == y2:
+                display.show(Image.ARROW_N)
+                if record[len(record) - 1] == "W":
+                    radio.send("R")
+                else:
+                    radio.send("L")
+                radio.send("F")
+                record += "N"
+            elif y1 > y2 and x1 == x2:
+                display.show(Image.ARROW_W)
+                if record[len(record) - 1] == "N":
+                    radio.send("L")
+                else:
+                    radio.send("R")
+                radio.send("F")
+                record += "W"
+            elif y1 < y2 and x1 == x2:
+                display.show(Image.ARROW_E)
+                if record[len(record) - 1] == "N":
+                    radio.send("R")
+                else:
+                    radio.send("L")
+                radio.send("F")
+                record += "E"
+            else:
+                continue
+            sleep(20)
 
 
 
