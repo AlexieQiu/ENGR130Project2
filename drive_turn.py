68a10c9a217e2fdba350697d7a88ee8dd15aece3
Alexie Qiu <qiu235@purdue.edu>
2025-03-07 12:32:42 -0500

    update python files

--- drive_turn.py
+++ drive_turn.py
@@ -8,17 +8,28 @@ M1B = 0x2
 M2A = 0x3
 M2B = 0x4
 
+M1A = 0x1
+M1B = 0x2
+M2A = 0x3
+M2B = 0x4
+
 r.setup()
 
 def forward_1():
     sleep(1000)
-    r.motor(M2B, 82.5)
+    r.motor(M2B, 83.5)
     r.motor(M1A, -90)
     sleep(730)
     r.motor(M2B, 0)
     r.motor(M1A, 0)
 
 def turn_left():
+    sleep(1000)
+    r.motor(M2B, 38)
+    r.motor(M1A, -40)
+    sleep(120)
+    r.motor(M2B, 0)
+    r.motor(M1A, 0)
     sleep(1000)
     r.motor(M2B, -90)
     r.motor(M1A, -90)
@@ -27,6 +38,12 @@ def turn_left():
     r.motor(M1A, 0)
 
 def turn_right():
+    sleep(1000)
+    r.motor(M2B, 38)
+    r.motor(M1A, -40)
+    sleep(120)
+    r.motor(M2B, 0)
+    r.motor(M1A, 0)
     sleep(1000)
     r.motor(M2B, 90)
     r.motor(M1A, 90)
